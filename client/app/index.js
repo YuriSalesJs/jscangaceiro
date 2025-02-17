1c128a807485c170240d5c4953059216062877a3
Yuri <yurisales2000@hotmail.com>
2025-02-17 11:56:50 -0300

    limpando dados + comentarios

--- client/app/index.js
+++ client/app/index.js
@@ -12,17 +12,25 @@ document.querySelector('.form').addEventListener('submit', function(event){
     event.preventDefault()
 
     var tr = document.createElement('tr')
-
+    //criando a tr onde serão mostrados os dados 
     campos.forEach(function(campo){
-
+    //criando a td com os dados dos campos 
         var td = document.createElement('td')
         td.textContent = campo.value
         tr.appendChild(td)
     })
 
+    //criando o ultimo dado da td
     var tdVolume = document.createElement('td')
     tdVolume.textContent = campos[1].value * campos[2].value
     tr.appendChild(tdVolume)
 
     tbody.appendChild(tr)
+
+    //limpando os dados dos campos para que o usuario possa incluir novos dados
+    campos[0].value = ''
+    campos[1].value = 1
+    campos[2].value = 0
+
+    campos[0].focus()
 })
