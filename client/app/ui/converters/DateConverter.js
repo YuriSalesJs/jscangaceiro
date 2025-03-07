602bca2a008a5298bdc1ac0f28a4a11282228b29
Yuri <yurisales2000@hotmail.com>
2025-03-07 16:58:51 -0300

    4.3 e 4.4

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -1,14 +1,16 @@
 class DateConverter {
     constructor()	{
-        throw new Error('Esta	classe	não	pode	ser	instanciada');
+        throw new Error('Esta classe não pod ser instanciada')
             }
 
     static paraTexto(data){
-        return data.getDate()
-        + '/' + (data.getMonth()  + 1)
-        + '/' + data.getFullYear()
+        return `${data.getDate()}/${data.getMonth()+ 1}/${data.getFullYear()}`
     }
-    static paraData(texto){
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2 )
-    )}
-}
+    static	paraData(texto)	{
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+        throw new Error('Deve	estar	no	formato	aaaa-mm-dd')
+                    return new Date(...texto.split('-').map((item,indice)	=>	
+        item	-	indice	%	2))
+    }
+        
+}  
